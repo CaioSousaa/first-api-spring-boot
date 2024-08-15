fd4731e6ecd3fbf013d463178ede7afc3f520c30
caio <caiothalles607@gmail.com>
2024-08-15 20:06:42 -0300

    feat: created DTO and data request for user

---
+++ src/main/java/com/example/basic_api/domain/user/UserRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.basic_api.domain.user;
+
+public record UserRequestDTO(String name, String email, String password) {
+}
---
+++ src/main/java/com/example/basic_api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.basic_api.repositories;
+
+import com.example.basic_api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
